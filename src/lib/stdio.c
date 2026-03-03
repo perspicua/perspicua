9e007727f9750ae3d016be34d5f3abf2a2de5987
andrei-riciu <andrei.riciu205@gmail.com>
2026-03-03 12:57:38 +0200

    added more types printf

--- src/lib/stdio.c
+++ src/lib/stdio.c
@@ -2,9 +2,9 @@
 #include "../driver/uart.h"
 #include <stdarg.h>
 
-static void print_unsigned_long(unsigned long n, int base)
+static void print_unsigned_long(uint64_t n, int base)
 {
-    char buf[32];
+    char buf[64];
     int i = 0;
 
     if (n == 0)
@@ -29,11 +29,11 @@ static void print_unsigned_long(unsigned long n, int base)
         uart_send(buf[--i]);
 }
 
-static void print_number(int num, int base)
+static void print_number(int32_t num, int base)
 {
     char buf[32];
     int i = 0;
-    unsigned int n;
+    uint32_t n;
     if (num < 0 && base == 10)
     {
         uart_send('-');
@@ -65,12 +65,17 @@ static void print_number(int num, int base)
     while (i > 0)
         uart_send(buf[--i]);
 }
-void printf(char* fmt, ...)
+
+void print_unsigned_int(uint32_t n, int base)
+{
+    print_unsigned_long(n, base);
+}
+void printf(const char* fmt, ...)
 {
     va_list args;
     va_start(args, fmt);
 
-    for (char* p = fmt; *p != '\0'; p++)
+    for (const char* p = fmt; *p != '\0'; p++)
     {
         if (*p == '%')
         {
@@ -101,6 +106,36 @@ void printf(char* fmt, ...)
                 int c = va_arg(args, int);
                 uart_send(c);
                 break;
+            }
+            case 'u':
+            {
+                unsigned int i = va_arg(args, unsigned int);
+                print_unsigned_long(i, 10);
+                break;
+            }
+            case 'l':
+            { 
+                p++;
+                switch(*p){
+                    case 'd':
+                    {
+                        long i = va_arg(args, long);
+                        print_number(i, 10);
+                        break;
+                    }
+                    case 'u':
+                    {
+                        unsigned long i = va_arg(args, unsigned long);
+                        print_unsigned_long(i, 10);
+                        break;
+                    }
+                    case 'x':
+                    {
+                        unsigned long i = va_arg(args, unsigned long);
+                        print_unsigned_long(i, 16);
+                        break;
+                    }
+                }
             }
             case '%':
             {
--- src/lib/stdio.h
+++ src/lib/stdio.h
@@ -1,6 +1,20 @@
 #ifndef _STDIO_H_
 #define _STDIO_H_
 
-void printf(char* fmt, ...);
+#include "types.h"
+
+// Formatted print to UART
+// Supported format specifiers:
+//   %d   - signed int (decimal)
+//   %u   - unsigned int (decimal)
+//   %x   - unsigned int (hex, lowercase)
+//   %ld  - signed long / int64_t (decimal)
+//   %lu  - unsigned long / uint64_t / size_t (decimal)
+//   %lx  - unsigned long (hex, lowercase)
+//   %p   - pointer (hex with 0x prefix)
+//   %s   - null-terminated string
+//   %c   - single character
+//   %%   - literal '%'
+void printf(const char* fmt, ...);
 
 #endif // _STDIO_H_
