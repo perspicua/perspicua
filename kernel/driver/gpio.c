7c11cd0cdedf66c131ac30c14b6ab96ce82d55e9
edybostina <egbostina@gmail.com>
2026-03-15 12:23:31 +0200

    reformat gpio

--- kernel/driver/gpio.c
+++ kernel/driver/gpio.c
@@ -1,25 +1,42 @@
+/*
+ * gpio.c - Implementation of the General Purpose Input/Output (GPIO) driver.
+ *
+ * This file handles register-level GPIO configuration for the BCM2711,
+ * including function selection and pull-up/down resistor settings.
+ */
+
 #include "driver/gpio.h"
-#include "devicetree/pht.h"
+
 #include "panic.h"
 #include "addr.h"
 
-volatile unsigned int* GPIO_GPFSEL0;
-volatile unsigned int* GPIO_GPPUPDN0;
+#include "devicetree/pht.h"
 
+/* GPIO Register Pointers (Static) */
+static volatile unsigned int* gpio_gpfsel0 = (void*)0;
+static volatile unsigned int* gpio_gppupdn0 = (void*)0;
+
+/*
+ * gpio_init - Discovers and initializes the GPIO base address from the hardware tree.
+ */
 void gpio_init(void)
 {
     struct pht_node* gpio_node = pht_find_device("gpio");
-    if (gpio_node == NULL)
+    if (gpio_node == (void*)0)
     {
         PANIC("[ GPIO ] Device node not found in hardware tree!\n");
     }
 
     uintptr_t vbase = P2V(gpio_node->address[0]);
 
-    GPIO_GPFSEL0 = (unsigned int*)(vbase + 0x00);
-    GPIO_GPPUPDN0 = (unsigned int*)(vbase + 0xE4);
+    // BCM2711 GPIO register offsets
+    gpio_gpfsel0 = (unsigned int*)(vbase + 0x00);
+    gpio_gppupdn0 = (unsigned int*)(vbase + 0xE4);
 }
 
+/*
+ * gpio_set_pin_function - Sets the function of a specific GPIO pin.
+ */
 void gpio_set_pin_function(unsigned int pin, unsigned int function)
 {
     if (pin > GPIO_MAX_PIN)
@@ -29,13 +46,17 @@ void gpio_set_pin_function(unsigned int pin, unsigned int function)
 
     unsigned int reg_index = pin / 10;
     unsigned int bit_offset = (pin % 10) * 3;
-    unsigned int current_val = GPIO_GPFSEL0[reg_index];
+    unsigned int current_val = gpio_gpfsel0[reg_index];
 
+    // Each pin function is represented by 3 bits
     current_val &= ~(0b111 << bit_offset);
     current_val |= (function << bit_offset);
-    GPIO_GPFSEL0[reg_index] = current_val;
+    gpio_gpfsel0[reg_index] = current_val;
 }
 
+/*
+ * gpio_set_pull - Configures the pull-up/down resistor for a GPIO pin.
+ */
 void gpio_set_pull(unsigned int pin, unsigned int pull)
 {
     if (pin > GPIO_MAX_PIN)
@@ -45,11 +66,11 @@ void gpio_set_pull(unsigned int pin, unsigned int pull)
 
     unsigned int reg_index = pin / 16;
     unsigned int bit_offset = (pin % 16) * 2;
-    unsigned int current_val = GPIO_GPPUPDN0[reg_index];
+    unsigned int current_val = gpio_gppupdn0[reg_index];
 
-    // clear 2 bits then set new pull state
+    // Each pin pull state is represented by 2 bits; clear then set
     current_val &= ~(0b11 << bit_offset);
     current_val |= (pull << bit_offset);
 
-    GPIO_GPPUPDN0[reg_index] = current_val;
+    gpio_gppupdn0[reg_index] = current_val;
 }
--- kernel/include/driver/gpio.h
+++ kernel/include/driver/gpio.h
@@ -1,8 +1,17 @@
-#ifndef _GPIO_H_
-#define _GPIO_H_
+/*
+ * gpio.h - Public API for the General Purpose Input/Output (GPIO) driver.
+ *
+ * This file defines the constants and functions for configuring GPIO pins,
+ * including setting pin functions and pull-up/down resistors.
+ */
 
+#ifndef PERSPICUA_DRIVER_GPIO_H
+#define PERSPICUA_DRIVER_GPIO_H
+
+/* Maximum number of GPIO pins on the BCM2711 */
 #define GPIO_MAX_PIN 57
 
+/* GPIO Function Select values */
 #define GPIO_FUNC_INPUT 0b000
 #define GPIO_FUNC_OUTPUT 0b001
 #define GPIO_FUNC_ALT0 0b100
@@ -12,13 +21,24 @@
 #define GPIO_FUNC_ALT4 0b011
 #define GPIO_FUNC_ALT5 0b010
 
-// pull resistor values
+/* GPIO Pull-up/down resistor values */
 #define GPIO_PUPDN_NONE 0b00
 #define GPIO_PUPDN_UP 0b01
 #define GPIO_PUPDN_DOWN 0b10
 
+/*
+ * gpio_init - Discovers and initializes the GPIO base address from the hardware tree.
+ */
 void gpio_init(void);
+
+/*
+ * gpio_set_pin_function - Sets the function of a specific GPIO pin.
+ */
 void gpio_set_pin_function(unsigned int pin, unsigned int function);
+
+/*
+ * gpio_set_pull - Configures the pull-up/down resistor for a GPIO pin.
+ */
 void gpio_set_pull(unsigned int pin, unsigned int pull);
 
-#endif // _GPIO_H_
+#endif /* PERSPICUA_DRIVER_GPIO_H */
